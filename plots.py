7922f6aeb9b973f0e7ca2197cf13a4c39951ea02
alexiswu <wuxinyi2000@gmail.com>
2023-09-11 23:24:33 -0400

    remade into a class for better pipeline handling

--- plots.py
+++ plots.py
@@ -1,56 +1,58 @@
 """
-Author: Neel Dhulipala
+Author: Neel Dhulipala, Alexis (Xinyi) Wu
 Project: Air Partners
 
 Prototype of static reporting pipeline. Used primarily for testing scripts (for now).
 """
 
 from calendar import weekday
-import sys
-from import_data import DataImporter
 from utils.create_plots import *
+from multiprocessing import Process
 
 
-# STATICS
-YEAR = int(sys.argv[1])
-MONTH = int(sys.argv[2])
 
-# Import sensor data
-di = DataImporter(year=YEAR, month=MONTH)
-sn_list, sn_dict = di.get_PM_data()
+class PlotPipeline():
+    def __init__(self, year,month, sn_list, sn_dict):
+        self.YEAR = year
+        self.MONTH = month
+        self.date_str = str(self.YEAR) + '-0' + str(self.MONTH) if self.MONTH <= 9 else str(self.YEAR) + '-' + str(self.MONTH)
+        self.sn_list = sn_list
+        self.sn_dict = sn_dict
 
 
-# create date string for data storage
-date_str = str(YEAR) + '-0' + str(MONTH) if MONTH<=9 else str(YEAR) + '-' + str(MONTH)
+    def plot_graphs(self):
+        pl = Plotter(self.date_str, self.sn_list, self.sn_dict)
 
-# plot graphs
-pl = Plotter(date_str, sn_list, sn_dict)
+        # Define the tasks to be run in parallel
+        tasks = [
+            lambda: pl.plot_and_export(calendar_plot, pm='pm1', month=self.MONTH, year=self.YEAR),
+            lambda: pl.plot_and_export(calendar_plot, pm='pm25', month=self.MONTH, year=self.YEAR),
+            lambda: pl.plot_and_export(calendar_plot, pm='pm10', month=self.MONTH, year=self.YEAR),
+            lambda: pl.plot_and_export(timeplot_threshold, pm=None),
+            lambda: pl.plot_and_export(diurnal_plot, pm='pm1', weekday=True),
+            lambda: pl.plot_and_export(diurnal_plot, pm='pm25', weekday=True),
+            lambda: pl.plot_and_export(diurnal_plot, pm='pm10', weekday=True),
+            lambda: pl.plot_and_export(diurnal_plot, pm='pm1', weekday=False),
+            lambda: pl.plot_and_export(diurnal_plot, pm='pm25', weekday=False),
+            lambda: pl.plot_and_export(diurnal_plot, pm='pm10', weekday=False),
+            lambda: pl.plot_and_export(wind_polar_plot, pm='pm1'),
+            lambda: pl.plot_and_export(wind_polar_plot, pm='pm25'),
+            lambda: pl.plot_and_export(wind_polar_plot, pm='pm10')
+        ]
 
-# # calendar plots
-pl.plot_and_export(calendar_plot, pm='pm1', month=MONTH, year=YEAR)
-pl.plot_and_export(calendar_plot, pm='pm25',month=MONTH, year=YEAR)
-pl.plot_and_export(calendar_plot, pm='pm10',month=MONTH, year=YEAR)
-print('Calendars plotted')
-plt.close()
 
-# timeplots with thresholds
-pl.plot_and_export(timeplot_threshold, pm=None)
-print('Timelines plotted')
-plt.close()
+        # Create and start a process for each task
+        processes = []
+        for task in tasks:
+            p = Process(target=task)
+            processes.append(p)
+            p.start()
 
-# diurnal plots
-pl.plot_and_export(diurnal_plot, pm='pm1', weekday=True)
-pl.plot_and_export(diurnal_plot, pm='pm25',weekday=True)
-pl.plot_and_export(diurnal_plot, pm='pm10',weekday=True)
-pl.plot_and_export(diurnal_plot, pm='pm1', weekday=False)
-pl.plot_and_export(diurnal_plot, pm='pm25',weekday=False)
-pl.plot_and_export(diurnal_plot, pm='pm10',weekday=False)
-print('Diurnals plotted')
-plt.close()
+        # Wait for all processes to finish
+        for p in processes:
+            p.join()
 
-# wind polar plots (computationally expensive)
-pl.plot_and_export(wind_polar_plot, pm='pm1')
-pl.plot_and_export(wind_polar_plot, pm='pm25')
-pl.plot_and_export(wind_polar_plot, pm='pm10')
-print('Wind polar plots plotted')
-plt.close()
+        print('All plots generated.')
+
+    def run(self):
+        self.plot_graphs()
