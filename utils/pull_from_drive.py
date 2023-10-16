a7c9a383028d3d979d6fd03d96cb68f845bed124
alexiswu <wuxinyi2000@gmail.com>
2023-10-16 17:35:03 -0400

    NO MORE NEED FOR TOKENSgit add utils/pull_from_drive.py !

--- utils/pull_from_drive.py
+++ utils/pull_from_drive.py
@@ -7,94 +7,31 @@ Script for setting/ refreshing google credentials and  pulling form data from go
 from __future__ import print_function
 import os
 import json
-import requests
-from datetime import datetime, timezone,timedelta
-from dateutil import parser
 
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
+
 
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
-TOKEN_PATH = 'creds/token.json' # Auto generated
 CREDENTIALS_PATH = 'creds/google_credentials.json' # Manually downloaded from google platform
 CONFIG_PATH = 'creds/google_config.json'
 AUTHORIZATION_URL = "https://oauth2.googleapis.com/token"
 
-def checkExpired():
-    """
-    Checks if the token has expired. If it has, deletes the token and returns True.
-    Please note that this token is different from google credentials and you should never manually change the token.json.
-    """
-    if not os.path.exists(TOKEN_PATH):
-        return True
-    with open(TOKEN_PATH, 'r') as f:
-        try:
-            creds = json.load(f)
-        except json.decoder.JSONDecoderError:
-            os.remove(TOKEN_PATH)
-            return True
-    now = datetime.now(timezone.utc)
-    exp_dt = parser.parse(creds['expiry'])
-    return exp_dt - now < timedelta(minutes=10)
-
-def refreshToken():
-    """
-    Refreshes the token if it has expired.
-    """
-    if not os.path.exists(TOKEN_PATH):
-        return None
-    
-    with open(TOKEN_PATH, 'r') as f:
-        creds = json.load(f)
-    
-    params = {
-            "grant_type": "refresh_token",
-            "client_id": creds["client_id"],
-            "client_secret": creds["client_secret"],
-            "refresh_token": creds["refresh_token"]
-    }
-
-    if checkExpired():
-        try:
-            r = requests.post(AUTHORIZATION_URL, data=params)
-            r.raise_for_status()
-            creds['token'] = r.json()['access_token']
-            with open(TOKEN_PATH, 'w') as f:
-                json.dump(creds, f)
-            return 1
-        except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
-            print(f"Error refreshing token: {e}")
-            return 0
-    else:
-        return None
-
-
 def load_credentials():
     """
-    Loads credentials from the token.json file. If the credentials are not valid, creates new credentials.
+    Loads credentials from the google_credentials.json file. If the credentials are not valid, creates new credentials.
     """
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(TOKEN_PATH):
-        creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            refreshToken()
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
-            creds = flow.run_local_server(port=0)
-        
-        with open(TOKEN_PATH, 'w') as token:
-            token.write(creds.to_json())
-    
+    if os.path.exists(CREDENTIALS_PATH):
+        creds =  service_account.Credentials.from_service_account_file(CREDENTIALS_PATH, scopes=SCOPES)
+        print(f"credentials loaded: {creds}")
+    else:
+        # we do not have the google_credentials.json file
+        print("No credentials file found. Please download the credentials file from the google platform.")
     return creds
+
 
 def load_config():
     """
@@ -109,12 +46,12 @@ def load_config():
             'maillist': '17GP7PlQYxr1A1_1srrSDpLjCplLztdHWG51XY2qZoVo',
             'sensor_install_data': '15DDTqQkXqD16vCnOBBTz9mPUmVWnKywjxdNWF9N6Gcg'
         }
+    
 
 def pull_sensor_install_data():
     """
     Pulls sensor install data from Google Drive.    
     """
-    refreshToken()
     creds = load_credentials()
     
     try:
